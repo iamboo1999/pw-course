b39e944a94aeb5a4013782ab10a8fdd6df9b5c21
tuong <nguyentuong199@gmail.com>
2025-04-10 14:36:41 +0700

    feat: add exercise lesson 5

--- tests/plw-syntax.spec.ts
+++ tests/plw-syntax.spec.ts
@@ -9,7 +9,7 @@ test( 'test case name', async ({ page }) => {
 
     await test.step('Click vào bài 1', async () => {
         await page.locator('//a[@href="01-xpath-register-page.html"]').click();
-    })
+    });
 
     await test.step('Fill information', async () => {
         await page.locator('//input[@id="username"]').fill('PlaywrightVN');
@@ -78,5 +78,73 @@ test( 'xử lý mouse event', async ({ page }) => {
         });
     })
 
+})
+
+test( 'Add to do list', async ({ page }) => {
+    // code của test
+
+    await test.step('ĐI đến trang to do page', async () => {
+        await page.goto('https://material.playwrightvn.com/');
+        await page. locator('//a[@href="03-xpath-todo-list.html"]'). click();
+    });
+
+    await test.step('Thêm mới 100 todo item có nội dung "Todo <i>"', async () => {
+        for (let i = 1; i <=100; i++) {
+            await page.locator('//input[@id="new-task"]').fill(`Todo ${i}`);
+            await page.locator('//button[@id="add-task"]').click();
+            await page.waitForTimeout(100);
+        }
+    });
+    debugger;
+
+    await test.step('Xóa các todo có số lẻ', async () => {
+        page.on('dialog', async dialog => {
+            await dialog.accept();
+        });
+
+        for (let i = 1; i <= 100; i++) {
+            if (i % 2 !== 0) {
+                await page.locator(`//button[@id="todo-${i}-delete"]`).click();
+                await page.waitForTimeout(100);
+            }
+        }
+    });
+    debugger;
+
+})
+
+test( 'Add personal notes', async ({ page }) => {
+    const notes = [
+        { title: 'Note 1', content: 'Content 1' },
+        { title: 'Note 2', content: 'Content 2' },
+        { title: 'Note 3', content: 'Content 3' },
+        { title: 'Note 4', content: 'Content 4' },
+        { title: 'Note 5', content: 'Content 5' },
+        { title: 'Note 6', content: 'Content 6' },
+        { title: 'Note 7', content: 'Content 7' },
+        { title: 'Note 8', content: 'Content 8' },
+        { title: 'Note 9', content: 'Content 9' },
+        { title: 'Note 10', content: 'Content 10' },
+    ];
+
+    await test.step('Đi đến trang personal notes', async () => {
+        await page.goto('https://material.playwrightvn.com/');
+        await page.locator('//a[@href="04-xpath-personal-notes.html"]').click();
+    });
+
+    await test.step('Thêm mới 10 note có nội dung là tiêu đề và nội dung', async () => {
+
+        for (const note of notes) {
+            await page.locator('//input[@id="note-title"]').fill(note.title);
+            await page.locator('//textarea[@id="note-content"]').fill(note.content);
+            await page.locator('//button[@id="add-note"]').click();
+            await page.waitForTimeout(100);
+        }
+    });
+
+    await test.step('Thực hiện search theo tiêu đề', async () => {
+        await page.locator('//input[@id="search"]').fill('1');
+        await page.waitForTimeout(100);
+    });
 
 })
